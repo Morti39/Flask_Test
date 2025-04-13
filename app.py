578ffba0aef569c73c49dc139a005b4392476511
Morti <mortiyt300908@gmail.com>
2025-04-13 23:30:43 +0300

    2

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ app = Flask(__name__)
 @app.route ("/")
 def index():
     return render_template("index.html")
-@app.route("/registration")
+@app.route("/contacts")
 def reg():
-    return render_template("registration.html")
-@app.route ("/profile")
+    return render_template("contacts.html")
+@app.route ("/about")
 def prof():
-    return  render_template("profile.html")
+    return  render_template("about.html")
+@app.route("/heroes")
+def comp():
+    return render_template('heroes.html')
 if __name__ == "__main__":
     app.run(debug=True)
